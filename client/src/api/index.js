5c216234fdcc43f19fdf3a12e58de632fcfad100
Saptarshi Hore <saptarshi0009@gmail.com>
2021-06-13 13:55:51 +0530

    backend API heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//const url = 'https://saps-memories-project.herokuapp.com/posts';
+const API = axios.create({ baseURL: 'https://saps-memories-project.herokuapp.com' });
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+//const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
